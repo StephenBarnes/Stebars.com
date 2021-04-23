5cd473d39b933b147ae3751d3a766755702e4d4f
J <stbarnes@stanford.edu>
2021-04-23 00:44:13 -0500

    Add DogBox link, remove starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>
+				<i>you can do anything at stebars.com</i>
+				</h1>
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<a
+					className="App-link"
+					href="https://dogbox.stebars.com"
+				>
+					DogBox
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
