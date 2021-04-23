8de2fd23afeb9b010d59df5181594dd3c3f3d8fc
J <stbarnes@stanford.edu>
2021-04-23 01:54:34 -0500

    Update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders DogBox link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/DogBox/i);
   expect(linkElement).toBeInTheDocument();
 });
